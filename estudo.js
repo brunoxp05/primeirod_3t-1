a7139a1a3fba9a01954d847bda73223c8e33fcd2
brunoxp05 <111583474+brunoxp05@users.noreply.github.com>
2022-10-07 12:03:31 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,38 +1,17 @@
 
-let tabuada = 2;
+lfution total(){
 
-function escreva(){
-    //tabuada do dois
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"</br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*9)+"<br>"); 
-    document.write(tabuada + " x 2 = "+(tabuada*10)+"<br>");
+    let c = document.getElementById("valor").valor;
+    let j = document.getElementById("juros").valor;
+    let t = document.getElementById("meses").valor;
+    let r = 0;
 
-    
+    for(let i =1; i <= t; i++  ){
+      let r = c * (1 +(j/100));
+      let v = r;
+     }
+   document.write("Resultado: " + r);
 }
-function repete(){
- for(let i = 1; i <= 10; i++){
-   document.write("tabuada do " + i + "</br>"); 
-     for(let j=1; j <= 10; j++){
-      document.write(i + " X " + j + "=" + (j*i) + "</br>" );
-      }
-   document.write("</br>"); 
- }
- } 
-
-let lista = ["Banana","Uva","Abacate","Tomate"]; 
-function minhaLista(){
- document.write(lista[1]) 
-}
-
-
-
 
 
 
